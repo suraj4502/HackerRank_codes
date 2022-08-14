9bde0fb57f5db5ed508b85f8359b98be6f476dcc
Suraj Yadav <76464630+suraj4502@users.noreply.github.com>
2022-08-14 16:40:18 +0530

    Update Changing_month_number_to_financial_month_number.py

--- Changing_month_number_to_financial_month_number.py
+++ Changing_month_number_to_financial_month_number.py
@@ -1,11 +1,16 @@
+
+def financial_month_convertor(month_no):
+    fmn=[]
+    for i in month_no:
+        if i<=3:
+            i=i+9
+            fmn.append(i)
+        else:
+            i = i-3
+            fmn.append(i)
+        return fmn
+   
 month_no = [1,2,3,4,5,6,7,8,9,10,11,12]
-fmn=[]
-for i in month_no:
-    if i<=3:
-        i=i+9
-        fmn.append(i)
-    else:
-        i = i-3
-        fmn.append(i)
-    
-print(fmn)
+result = financial_month_convertor(month_no)
+
+print(result)
