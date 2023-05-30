063d59b712126f13f7d432a7a6034231af5e9435
Surajkumar Yadav <76464630+suraj4502@users.noreply.github.com>
2023-05-30 12:57:44 +0530

    np.min and np.max

---
+++ np_min_max.py
@@ -0,0 +1,35 @@
+import numpy as np
+# Compute the min along axis 1 and then print the max of that result.
+n,m = map(int,input().split())
+
+arr = np.array([input().split() for _ in range(n)],int)
+
+print(np.max(np.min(arr,axis=1)))
+
+
+'''
+import numpy
+
+my_array = numpy.array([[2, 5], 
+                        [3, 7],
+                        [1, 3],
+                        [4, 0]])
+
+print numpy.max(my_array, axis = 0)         #Output : [4 7]
+print numpy.max(my_array, axis = 1)         #Output : [5 7 3 4]
+print numpy.max(my_array, axis = None)      #Output : 7
+print numpy.max(my_array)                   #Output : 7
+
+
+my_array = numpy.array([[2, 5], 
+                        [3, 7],
+                        [1, 3],
+                        [4, 0]])
+
+print numpy.min(my_array, axis = 0)         #Output : [1 0]
+print numpy.min(my_array, axis = 1)         #Output : [2 3 1 0]
+print numpy.min(my_array, axis = None)      #Output : 0
+print numpy.min(my_array)                   #Output : 0
+'''
+
+
