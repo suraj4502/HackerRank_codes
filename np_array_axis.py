f8911ddc8b305661e60d0df0c4bf4a3a89b4f84f
Surajkumar Yadav <76464630+suraj4502@users.noreply.github.com>
2023-05-30 12:30:34 +0530

    np.sum and np.prod

---
+++ np_array_axis.py
@@ -0,0 +1,33 @@
+import numpy as np
+#  our task is to first find sum over axis =0 and then find product
+
+n,m = map(int,input().split())
+
+arr = np.array([input().split() for _ in range(n)],int)
+
+print(np.prod(np.sum(arr,axis=0)))
+
+
+
+
+'''
+import numpy
+
+my_array = numpy.array([ [1, 2], [3, 4] ])
+
+print numpy.sum(my_array, axis = 0)         #Output : [4 6]
+print numpy.sum(my_array, axis = 1)         #Output : [3 7]
+print numpy.sum(my_array, axis = None)      #Output : 10
+print numpy.sum(my_array)                   #Output : 10
+
+-----------------------------------------------------------
+import numpy
+
+my_array = numpy.array([ [1, 2], [3, 4] ])
+
+print numpy.prod(my_array, axis = 0)            #Output : [3 8]
+print numpy.prod(my_array, axis = 1)            #Output : [ 2 12]
+print numpy.prod(my_array, axis = None)         #Output : 24
+print numpy.prod(my_array)                      #Output : 24
+
+'''
