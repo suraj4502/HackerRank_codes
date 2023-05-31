50a09a6e6c38f999a5e3f6f679cdc32f2db185f2
Surajkumar Yadav <76464630+suraj4502@users.noreply.github.com>
2023-05-31 23:55:58 +0530

    printing integers patterns

---
+++ patterns.py
@@ -0,0 +1,19 @@
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(((10**i)//9)*i)
+   
+   
+   
+   
+   '''
+   Sample Input
+
+5
+
+
+Sample Output
+
+1
+22
+333
+4444
+'''
