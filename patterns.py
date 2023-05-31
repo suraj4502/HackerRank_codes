94ee5378b7a30a7f56339e92edea6d1f9d2a7ad8
Surajkumar Yadav <76464630+suraj4502@users.noreply.github.com>
2023-06-01 00:01:04 +0530

    printing integers patterns

--- patterns.py
+++ patterns.py
@@ -16,4 +16,12 @@ Sample Output
 22
 333
 4444
+
+
+
+
+code explanation:
+For i = 1, 10**1 is 10. Dividing 10 by 9 gives 1 (integer division), and multiplying it by 1 gives 1. So, 1 is printed.
+For i = 2, 10**2 is 100. Dividing 100 by 9 gives 11 (integer division), and multiplying it by 2 gives 22. So, 22 is printed.
+For i = 3, 10**3 is 1000. Dividing 1000 by 9 gives 111 (integer division), and multiplying it by 3 gives 333. So, 333 is printed.
 '''
